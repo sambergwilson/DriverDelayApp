9c2a600df15aec7b967a2233a62f54912bf0854e
Cortney Wilson <wilson84.cw@gmail.com>
2023-11-14 14:39:43 -0500

    Add project files.

---
+++ DriverDelayApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DriverDelayApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ DriverDelayApp/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace YourAppName
+{
+    public partial class MainPage : ContentPage
+    {
+        private int totalDelayMinutes = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnAddStopClicked(object sender, EventArgs e)
+        {
+            // Calculate the delay for the current stop
+            TimeSpan arrivedTime = arrivedTimePicker.Time;
+            TimeSpan departedTime = departedTimePicker.Time;
+            int paidMinutes = int.Parse(paidEntry.Text);
+
+            int delayMinutes = (int)(departedTime - arrivedTime).TotalMinutes - paidMinutes;
+            totalDelayMinutes += delayMinutes;
+
+            // Update the total time delayed label
+            totalTimeLabel.Text = totalDelayMinutes.ToString() + " minutes";
+        }
+
+        private void OnRemoveStopClicked(object sender, EventArgs e)
+        {
+            // Remove the delay for the last stop (if any)
+            totalDelayMinutes = Math.Max(0, totalDelayMinutes);
+
+            // Update the total time delayed label
+            totalTimeLabel.Text = totalDelayMinutes.ToString() + " minutes";
+        }
+    }
+}
